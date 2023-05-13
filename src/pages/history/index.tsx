6651bcf7fcefe115fd9b2ffc1913ffa99d807a87
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2023-05-12 22:06:11 -0300

    Feat: Implement First Layout For History Screen

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,3 +1,70 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
